e487746af9bf2f72604c4ea718635424f0a90c11
Ayman20184015 <ayman200051@hotmail.com>
2026-05-19 01:17:50 +0300

    Commit 3: setup caching

--- server.py
+++ server.py
@@ -1,9 +1,29 @@
 import socket
+import json
+from meal_api import MealAPI
 
 class RecipeServer:
     def __init__(self):
         self.host = '127.0.0.1'
         self.port = 5555
+        self.cache = self.startup_cache()
+
+    def startup_cache(self):
+        print("[SERVER] Initializing reference cache...")
+        cats = MealAPI.get_categories()
+        areas = MealAPI.get_areas()
+        ingr = MealAPI.get_ingredients()
+        
+        cache_data = {
+            "categories": cats,
+            "areas": areas,
+            "ingredients": {"meals": ingr.get("meals", [])[:50]} 
+        }
+        
+        with open("reference.json", "w") as f:
+            json.dump(cache_data, f, indent=4)
+        print("[SERVER] Reference file created locally.")
+        return {"categories": cats, "areas": areas, "ingredients": ingr}
 
     def start(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
