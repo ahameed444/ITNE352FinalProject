b0b75f79d6c63953b75792b28dcbab3f781ba740
Ayman20184015 <ayman200051@hotmail.com>
2026-05-19 09:19:21 +0300

    Commit 4: setup threading

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 import json
 from meal_api import MealAPI
 
@@ -13,23 +14,33 @@ class RecipeServer:
         cats = MealAPI.get_categories()
         areas = MealAPI.get_areas()
         ingr = MealAPI.get_ingredients()
-        
-        cache_data = {
-            "categories": cats,
-            "areas": areas,
-            "ingredients": {"meals": ingr.get("meals", [])[:50]} 
-        }
-        
+        cache_data = {"categories": cats, "areas": areas, "ingredients": {"meals": ingr.get("meals", [])[:50]}}
         with open("reference.json", "w") as f:
             json.dump(cache_data, f, indent=4)
-        print("[SERVER] Reference file created locally.")
         return {"categories": cats, "areas": areas, "ingredients": ingr}
+
+    def handle_client(self, conn, addr):
+        try:
+            username = conn.recv(1024).decode()
+            print(f"[NEW CONNECTION] {addr} connected. User: {username}")
+            while True:
+                data = conn.recv(4096).decode()
+                if not data or data == "QUIT":
+                    break
+        except Exception as e:
+            print(f"[ERROR] {e}")
+        finally:
+            conn.close()
 
     def start(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((self.host, self.port))
         server.listen(5)
-        print(f"[LISTENING] Server active on {self.host}:{self.port}...")
+        print(f"[LISTENING] Server active...")
+        while True:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
 
 if __name__ == "__main__":
     RecipeServer().start()
